dc41d87072ab69dff7676e6a47df9ed15f0a76b2
Indy <116882977+indyyd@users.noreply.github.com>
2022-11-25 11:28:55 +0000

    Add files via upload
    basic movement of a circle

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,22 @@
+var x, y, d;
+
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
+    x = width/2;
+    y = height/2;
+    d = 200;
 }
 
 function draw() {
     background(220);
 
+    y++; //increases y by 1 every 60 frames per second
+    
+    if(y > height+d/2) {
+        y = -d/2;
+    }
+
     fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    ellipse(x, y, d);
 }
