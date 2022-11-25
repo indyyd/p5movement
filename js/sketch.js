da772593af842553f4fcf80f75a3db77824785d0
Indy <116882977+indyyd@users.noreply.github.com>
2022-11-25 11:35:43 +0000

    Add files via upload
    user interactivity with movement

--- js/sketch.js
+++ js/sketch.js
@@ -1,18 +1,23 @@
-var x, y, d;
+var x, y, d, speed;
 
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
     x = width/2;
     y = height/2;
-    d = 200;
+    d = 200; //the diameter of the circle
+    speed = 5;
 }
 
 function draw() {
     background(220);
 
-    y++; //increases y by 1 every 60 frames per second
+    //y++ increases y by 1 every 60 frames per second
     
+    speed = map(mouseY, 0, height, 5, 50);
+
+    y = y + speed;
+
     if(y > height+d/2) {
         y = -d/2;
     }
